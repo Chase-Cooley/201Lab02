01eb26154031003a0aa05bf01d365433c6dae31d
Chase-Cooley <cacoolsecondary@gmail.com>
2022-01-27 00:37:06 +0000

    Hello World 1st Try

---
+++ helloworld.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Script Name:                  My First Bash Script 
+# Class Name:                   Ops 201
+# Author Name:                  Chase Cooley
+# Date of latest revision:      1/26/2022
+# Purpose:                      Creating a bash script with a variable 
+
+# What is a variable?
+
+# Declaration
+my_variable=$(ip a)
+
+# Call
+echo $my_variable
+
+# echo $(ip a)
+
+ touch output.txt
